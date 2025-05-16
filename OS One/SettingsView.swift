e254cc565c29ef10aaf31e412219dbcaf831c745
sighmon <simon.loffler@gmail.com>
2025-05-16 14:40:53 +0930

    #2 Embed the settings view in a scroll view now that we have lots of settings

--- OS One/SettingsView.swift
+++ OS One/SettingsView.swift
@@ -26,150 +26,152 @@ struct SettingsView: View {
         NavigationView {
             ZStack {
                 Color(red: 0.5, green: 0.5, blue: 0.5).edgesIgnoringSafeArea(.all).opacity(0.1)
-                VStack {
-                    HStack {
-                        Text("OS")
-                            .font(.system(
-                                size: 50,
-                                weight: .light
-                            ))
-                        Text("1")
-                            .font(.system(
-                                size: 30,
-                                weight: .regular
-                            ))
-                            .baselineOffset(20.0)
-                    }
-                    Text("settings")
-                        .font(.system(size: 25, weight: .light))
-                        .padding(.bottom, 5)
-                    Text(appVersionAndBuild())
-                        .font(.system(size: 15, weight: .light))
-                        .padding(.bottom, 10)
-                    Group {
-                        SecureField("OpenAI API Key", text: $openAIApiKey)
-                            .onChange(of: openAIApiKey) {
-                                UserDefaults.standard.set($0, forKey: "openAIApiKey")
+                ScrollView {
+                    VStack {
+                        HStack {
+                            Text("OS")
+                                .font(.system(
+                                    size: 50,
+                                    weight: .light
+                                ))
+                            Text("1")
+                                .font(.system(
+                                    size: 30,
+                                    weight: .regular
+                                ))
+                                .baselineOffset(20.0)
+                        }
+                        Text("settings")
+                            .font(.system(size: 25, weight: .light))
+                            .padding(.bottom, 5)
+                        Text(appVersionAndBuild())
+                            .font(.system(size: 15, weight: .light))
+                            .padding(.bottom, 10)
+                        Group {
+                            SecureField("OpenAI API Key", text: $openAIApiKey)
+                                .onChange(of: openAIApiKey) {
+                                    UserDefaults.standard.set($0, forKey: "openAIApiKey")
+                                }
+                            SecureField("OpenAI Session Key (optional)", text: $openAISessionKey)
+                                .onChange(of: openAISessionKey) {
+                                    UserDefaults.standard.set($0, forKey: "openAISessionKey")
+                                }
+                            SecureField("Eleven Labs API Key", text: $elevenLabsApiKey)
+                                .onChange(of: elevenLabsApiKey) {
+                                    UserDefaults.standard.set($0, forKey: "elevenLabsApiKey")
+                                }
+                        }
+                        Toggle("Allow location", isOn: $allowLocation)
+                            .onChange(of: allowLocation) {
+                                UserDefaults.standard.set($0, forKey: "allowLocation")
                             }
-                        SecureField("OpenAI Session Key (optional)", text: $openAISessionKey)
-                            .onChange(of: openAISessionKey) {
-                                UserDefaults.standard.set($0, forKey: "openAISessionKey")
+                        Toggle("Allow search", isOn: $allowSearch)
+                            .onChange(of: allowSearch) {
+                                UserDefaults.standard.set($0, forKey: "allowSearch")
                             }
-                        SecureField("Eleven Labs API Key", text: $elevenLabsApiKey)
-                            .onChange(of: elevenLabsApiKey) {
-                                UserDefaults.standard.set($0, forKey: "elevenLabsApiKey")
+                        Toggle("GPT-4.1", isOn: $gpt4)
+                            .onChange(of: gpt4) {
+                                UserDefaults.standard.set($0, forKey: "gpt4")
+                            }
+                        Toggle("OpenAI voice", isOn: $openAIVoice)
+                            .onChange(of: openAIVoice) {
+                                UserDefaults.standard.set($0, forKey: "openAIVoice")
+                            }
+                        if openAISessionKey != "" {
+                            ProgressView(value: openAIUsage / 1000) {
+                                Text("$\((openAIUsage / 100), specifier: "%.2f")")
+                            }
+                            .padding(.bottom, 10)
+                        }
+                        Toggle("Eleven Labs voice", isOn: $elevenLabs)
+                            .onChange(of: elevenLabs) {
+                                UserDefaults.standard.set($0, forKey: "elevenLabs")
+                            }
+                        ProgressView(value: elevenLabsUsage) {
+                            Text("\(floatToPercent(float:elevenLabsUsage))")
+                                .opacity(elevenLabs ? 1.0 : 0.5)
+                        }
+                        Picker("Name of your voice assistant", selection: $name) {
+                            Group {
+                                Text("Samantha").tag("Samantha")
+                                Text("KITT").tag("KITT")
+                                Text("Mr.Robot").tag("Mr.Robot")
+                                Text("Elliot").tag("Elliot")
+                                Text("GLaDOS").tag("GLaDOS")
+                                Text("Spock").tag("Spock")
+                                Text("The Oracle").tag("The Oracle")
+                                Text("Janet").tag("Janet")
+                            }
+                            Group {
+                                Text("Ava").tag("Ava")
+                                Text("Darth Vader").tag("Darth Vader")
+                                Text("Johnny Five").tag("Johnny Five")
+                                Text("J.A.R.V.I.S.").tag("J.A.R.V.I.S.")
+                            }
+                            Group {
+                                Text("Amy Remeikis").tag("Amy Remeikis")
+                                Text("Jane Caro").tag("Jane Caro")
+                            }
+                            Group {
+                                Text("Martha Wells").tag("Murderbot")
+                            }
+                            Group {
+                                Text("Fei-Fei Li").tag("Fei-Fei Li")
+                                Text("Andrew Ng").tag("Andrew Ng")
+                                Text("Corinna Cortes").tag("Corinna Cortes")
+                                Text("Andrej Karpathy").tag("Andrej Karpathy")
+                            }
+                            Group {
+                                Text("Judith Butler").tag("Butler")
+                                Text("Noam Chomsky").tag("Chomsky")
+                                Text("Angela Davis").tag("Davis")
+                                Text("Slavoj Žižek").tag("Žižek")
+                            }
+                            Group {
+                                Text("Seb Chan").tag("Seb Chan")
+                            }
+                        }
+                            .pickerStyle(.wheel)
+                            .onChange(of: name) {
+                                UserDefaults.standard.set($0, forKey: "name")
                             }
                     }
-                    Toggle("Allow location", isOn: $allowLocation)
-                        .onChange(of: allowLocation) {
-                            UserDefaults.standard.set($0, forKey: "allowLocation")
-                        }
-                    Toggle("Allow search", isOn: $allowSearch)
-                        .onChange(of: allowSearch) {
-                            UserDefaults.standard.set($0, forKey: "allowSearch")
-                        }
-                    Toggle("GPT-4.1", isOn: $gpt4)
-                        .onChange(of: gpt4) {
-                            UserDefaults.standard.set($0, forKey: "gpt4")
-                        }
-                    Toggle("OpenAI voice", isOn: $openAIVoice)
-                        .onChange(of: openAIVoice) {
-                            UserDefaults.standard.set($0, forKey: "openAIVoice")
-                        }
-                    if openAISessionKey != "" {
-                        ProgressView(value: openAIUsage / 1000) {
-                            Text("$\((openAIUsage / 100), specifier: "%.2f")")
-                        }
-                        .padding(.bottom, 10)
-                    }
-                    Toggle("Eleven Labs voice", isOn: $elevenLabs)
-                        .onChange(of: elevenLabs) {
-                            UserDefaults.standard.set($0, forKey: "elevenLabs")
-                        }
-                    ProgressView(value: elevenLabsUsage) {
-                        Text("\(floatToPercent(float:elevenLabsUsage))")
-                            .opacity(elevenLabs ? 1.0 : 0.5)
-                    }
-                    Picker("Name of your voice assistant", selection: $name) {
-                        Group {
-                            Text("Samantha").tag("Samantha")
-                            Text("KITT").tag("KITT")
-                            Text("Mr.Robot").tag("Mr.Robot")
-                            Text("Elliot").tag("Elliot")
-                            Text("GLaDOS").tag("GLaDOS")
-                            Text("Spock").tag("Spock")
-                            Text("The Oracle").tag("The Oracle")
-                            Text("Janet").tag("Janet")
-                        }
-                        Group {
-                            Text("Ava").tag("Ava")
-                            Text("Darth Vader").tag("Darth Vader")
-                            Text("Johnny Five").tag("Johnny Five")
-                            Text("J.A.R.V.I.S.").tag("J.A.R.V.I.S.")
-                        }
-                        Group {
-                            Text("Amy Remeikis").tag("Amy Remeikis")
-                            Text("Jane Caro").tag("Jane Caro")
-                        }
-                        Group {
-                            Text("Martha Wells").tag("Murderbot")
-                        }
-                        Group {
-                            Text("Fei-Fei Li").tag("Fei-Fei Li")
-                            Text("Andrew Ng").tag("Andrew Ng")
-                            Text("Corinna Cortes").tag("Corinna Cortes")
-                            Text("Andrej Karpathy").tag("Andrej Karpathy")
-                        }
-                        Group {
-                            Text("Judith Butler").tag("Butler")
-                            Text("Noam Chomsky").tag("Chomsky")
-                            Text("Angela Davis").tag("Davis")
-                            Text("Slavoj Žižek").tag("Žižek")
-                        }
-                        Group {
-                            Text("Seb Chan").tag("Seb Chan")
-                        }
-                    }
-                        .pickerStyle(.wheel)
-                        .onChange(of: name) {
-                            UserDefaults.standard.set($0, forKey: "name")
-                        }
-                }
-                .textFieldStyle(.roundedBorder)
-                .textInputAutocapitalization(.never)
-                .disableAutocorrection(true)
-                .padding([.leading, .trailing], 40)
-                .onAppear {
-                    // Load OS One settings from user defaults
-                    openAIApiKey = UserDefaults.standard.string(forKey: "openAIApiKey") ?? ""
-                    openAISessionKey = UserDefaults.standard.string(forKey: "openAISessionKey") ?? ""
-                    gpt4 = UserDefaults.standard.bool(forKey: "gpt4")
-                    vision = UserDefaults.standard.bool(forKey: "vision")
-                    openAIVoice = UserDefaults.standard.bool(forKey: "openAIVoice")
-                    allowLocation = UserDefaults.standard.bool(forKey: "allowLocation")
-                    allowSearch = UserDefaults.standard.bool(forKey: "allowSearch")
-                    elevenLabsApiKey = UserDefaults.standard.string(forKey: "elevenLabsApiKey") ?? ""
-                    elevenLabs = UserDefaults.standard.bool(forKey: "elevenLabs")
-                    name = UserDefaults.standard.string(forKey: "name") ?? ""
+                    .textFieldStyle(.roundedBorder)
+                    .textInputAutocapitalization(.never)
+                    .disableAutocorrection(true)
+                    .padding([.leading, .trailing], 40)
+                    .onAppear {
+                        // Load OS One settings from user defaults
+                        openAIApiKey = UserDefaults.standard.string(forKey: "openAIApiKey") ?? ""
+                        openAISessionKey = UserDefaults.standard.string(forKey: "openAISessionKey") ?? ""
+                        gpt4 = UserDefaults.standard.bool(forKey: "gpt4")
+                        vision = UserDefaults.standard.bool(forKey: "vision")
+                        openAIVoice = UserDefaults.standard.bool(forKey: "openAIVoice")
+                        allowLocation = UserDefaults.standard.bool(forKey: "allowLocation")
+                        allowSearch = UserDefaults.standard.bool(forKey: "allowSearch")
+                        elevenLabsApiKey = UserDefaults.standard.string(forKey: "elevenLabsApiKey") ?? ""
+                        elevenLabs = UserDefaults.standard.bool(forKey: "elevenLabs")
+                        name = UserDefaults.standard.string(forKey: "name") ?? ""
 
-                    if (elevenLabsApiKey != "" && elevenLabs) {
-                        elevenLabsGetUsage { result in
-                            switch result {
-                            case .success(let usage):
-                                elevenLabsUsage = usage
-                            case .failure(let error):
-                                print("Eleven Labs API error: \(error.localizedDescription)")
+                        if (elevenLabsApiKey != "" && elevenLabs) {
+                            elevenLabsGetUsage { result in
+                                switch result {
+                                case .success(let usage):
+                                    elevenLabsUsage = usage
+                                case .failure(let error):
+                                    print("Eleven Labs API error: \(error.localizedDescription)")
+                                }
                             }
                         }
-                    }
 
-                    if (openAISessionKey != "") {
-                        getOpenAIUsage { result in
-                            switch result {
-                            case .success(let usage):
-                                openAIUsage = usage
-                            case .failure(let error):
-                                print("OpenAI API error: \(error.localizedDescription)")
+                        if (openAISessionKey != "") {
+                            getOpenAIUsage { result in
+                                switch result {
+                                case .success(let usage):
+                                    openAIUsage = usage
+                                case .failure(let error):
+                                    print("OpenAI API error: \(error.localizedDescription)")
+                                }
                             }
                         }
                     }
